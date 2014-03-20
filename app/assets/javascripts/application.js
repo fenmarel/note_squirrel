40489fb01c899d4b819b1c4f56d036a23ede8f2e
fenmarel <jonny.p.tatoes@gmail.com>
2014-03-20 16:28:13 -0700

    cleaned up navigation a bit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.serializeJSON
+//= require bootstrap
 //= require turbolinks
 //= require underscore
 //= require backbone
--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,13 @@
 class DashboardsController < ApplicationController
+  before_action :ensure_signed_in
+
   def index
     render :index
+  end
+
+  private
+
+  def ensure_signed_in
+    redirect_to new_user_session_url unless user_signed_in?
   end
 end
