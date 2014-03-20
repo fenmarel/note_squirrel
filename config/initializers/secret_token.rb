279cc261e8e14d4d4f0d3b73b9cdc3ae81f0c34f
fenmarel <jonny.p.tatoes@gmail.com>
2014-03-20 11:52:28 -0700

    initial with devise generated users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NoteSquirrel::Application.config.secret_key_base = 'a3851ae91fc359043a09bbc0ecc415c9559f7d9fd14812f65fa5dcfda795f009cc202987c81c1dcda4d3295fe20064f790b1d98a3dddc1e18097e69e7b88cd8f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NoteSquirrel::Application.config.session_store :cookie_store, key: '_note_squirrel_session'
